bd9df1c332060c8059e38685b7d947f7dd6b506f
mrandolph83 <mrandolph83@gmail.com>
2021-07-20 23:16:57 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+let totalBatteries = batteryBatches.reduce(function(passedIn, battery){
+    return passedIn + battery
+}, 0);
 // Code your solution here
